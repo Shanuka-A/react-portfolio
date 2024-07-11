d30920b57beb25e931bd6c4b1d89e0e59212ea2a
Shanuka Udara <it21302176@my.sliit.lk>
2024-07-11 14:47:25 +0530

    444

--- src/utils.js
+++ src/utils.js
@@ -1,16 +1,17 @@
 // src/utils.js
-import { generate } from 'vcard';
+import vCard from 'vcard-generator';
 import { saveAs } from 'file-saver';
 
 export const generateVCard = (name, phoneNumber, email, address) => {
-  const vCard = generate({
-    version: '3.0',
-    name: { firstNames: name },
-    telephone: phoneNumber,
-    email: email,
-    address: { street: address },
-  });
+  const card = vCard();
 
-  const blob = new Blob([vCard], { type: 'text/vcard;charset=utf-8' });
+  card
+    .addName(name)
+    .addPhoneNumber(phoneNumber, 'HOME')
+    .addEmail(email)
+    .addAddress(address);
+
+  const vCardData = card.toString('3.0');
+  const blob = new Blob([vCardData], { type: 'text/vcard;charset=utf-8' });
   saveAs(blob, 'contact.vcf');
 };
